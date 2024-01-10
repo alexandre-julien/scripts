7e23270c30366f8f54720d49713be48d98b089a8
Alexandre JULIEN <contact@alexandrejulien.com>
2024-01-10 08:20:19 +0100

    [wm_name] add pomodoro remaining time

--- wm_name.sh
+++ wm_name.sh
@@ -3,8 +3,12 @@
 bat="$(acpi | grep -o -E '[0-9]+%')"
 date="$(date "+%F %R")"
 pomo_status=$(cat ~/.pomodoro_status)
+pomo_time=$(cat ~/.pomodoro_remaining_time)
+if [ "$pomo_time" != "" ]; then
+	pomo_time="$pomo_time""m "
+fi
 
-echo " $date | $bat | Pomodoro: $pomo_status"
+echo " $date | $bat | Pomodoro: $pomo_status $pomo_time"
 
 # cron
 # * * * * * env DISPLAY=:0 xprop -root -set WM_NAME "$(/home/im7ffy/scripts/wm_name.sh)"
