e9bb9e854ede7aec4830114c5f820d3d41544e05
Alexandre JULIEN <contact@alexandrejulien.com>
2024-01-13 18:28:36 +0100

    [shelltime] implement stopwatch mode

--- shelltime.sh
+++ shelltime.sh
@@ -17,20 +17,29 @@ if [[ "$seconds" == *[[:alpha:]]* ]]; then
 	seconds=0
 fi
 
-# TODO: implement stopwatch mode
-# time=0
-# if [ "$arg" = "" ]; then
-# 	while true; do
-# 		echo -ne "\r\e[2K""$hours""h" "$minutes""m" "$i""s"
-# 		sleep 1
-# 		time=$((time+1))
-# 	done
-# fi
+if [ "$arg" = "" ]; then
+	hours=0
+	minutes=0
+	seconds=0
+	while true; do
+		if [ "$seconds" -ge 60 ]; then
+			minutes=$((minutes+1))
+			seconds=0
+		fi
+		if [ "$minutes" -ge 60 ]; then
+			minutes=0
+			hours=$((hours+1))
+		fi
+		echo -ne "\r\e[2K$hours""h""$minutes""m""$seconds""s"
+		sleep 1
+		seconds=$((seconds+1))
+	done
+fi
 
 while [ "$hours" -ge 0 ]; do
 	while [ "$minutes" -ge 0 ]; do
 		for ((i=seconds; i>0; i--)); do
-			echo -ne "\r\e[2K""$hours""h" "$minutes""m" "$i""s"
+			echo -ne "\r\e[2K$hours h$minutes m $i s"
 			sleep 1
 		done
 		minutes=$((minutes-1))
